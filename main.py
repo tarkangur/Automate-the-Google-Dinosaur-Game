33531bef7d6b7d9e51df47d5b8c345fcdfdda7f0
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-02-21 21:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-##
+from PIL import Image, ImageGrab
+import pyautogui
+import time
+
+
+def screenshot():
+    ss = ImageGrab.grab(bbox=(400, 600, 600, 900)).convert("L")
+    return ss
+
+
+def dark_theme(ss):
+    ss_data = ss.load()
+    if ss_data[0, 0] < 50:
+        return True
+    elif 170 <= ss_data[0, 0] < 200:
+        return False
+
+
+def check_obstacle(theme, ss):
+    ss_data = ss.load()
+
+
+def game():
+    while True:
+        ss = screenshot()
+        theme = "Dark" if dark_theme(ss) else "Light"
+        check_obstacle(theme, ss)
+        time.sleep(0.1)
+
+
+time.sleep(5)
+#game()
