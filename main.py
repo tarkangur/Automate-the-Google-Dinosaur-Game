794b0a0c60d345603ba91b32b0c797b24cc02637
Tarkan <142388310+tarkangur@users.noreply.github.com>
2025-02-22 22:21:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-from PIL import Image, ImageGrab
+from PIL import ImageOps, ImageGrab
 import pyautogui
 import time
+import keyboard
+
+screen_coords = (115, 310, 195, 380)
+obstacle_coords = (600, 780, 660, 880)
 
 
-def screenshot():
-    ss = ImageGrab.grab(bbox=(400, 600, 600, 900)).convert("L")
-    return ss
+def obstacle_screenshot():
+    img = ImageGrab.grab(bbox=obstacle_coords)
+    gray_img = ImageOps.grayscale(img)
+    obstacle_sum = sum(map(sum, gray_img.getcolors()))
+ #   print(obstacle_sum)
+    return obstacle_sum
 
 
-def dark_theme(ss):
-    ss_data = ss.load()
-    if ss_data[0, 0] < 50:
-        return True
-    elif 170 <= ss_data[0, 0] < 200:
-        return False
-
-
-def check_obstacle(theme, ss):
-    ss_data = ss.load()
+def screen_screenshot():
+    screen = ImageGrab.grab(bbox=screen_coords)
+    gray_screen = ImageOps.grayscale(screen)
+    screen_sum = sum(map(sum, gray_screen.getcolors()))
+#    print(screen_sum)
+    return screen_sum
 
 
 def game():
     while True:
-        ss = screenshot()
-        theme = "Dark" if dark_theme(ss) else "Light"
-        check_obstacle(theme, ss)
-        time.sleep(0.1)
+        obstacle = obstacle_screenshot()
+        screen = screen_screenshot()
+        if keyboard.is_pressed("q"):
+            break
+        if screen == 5855:
+            if obstacle != 6255:
+                pyautogui.press("space")
+                time.sleep(0.1)
+        elif screen == 5633:
+            if obstacle != 6033:
+                pyautogui.press("space")
+                time.sleep(0.1)
 
 
-time.sleep(5)
-#game()
+#  time.sleep(5)
+#  obstacle_screenshot()  # 6255=Light mode,  6033=Dark mode
+#  screen_screenshot()  # 5855=Light mode, 5633=Dark mode
+game()
